55ee4520c215a8b74a2139dd6c2b3d4645670d1a
mirnamp <ampueromirna8@gmail.com>
2022-11-10 13:37:00 -0500

    add functionality

---
+++ modules/elements-html.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.books-container');
+const addNew = document.querySelector('.add-new');
+const contact = document.querySelector('.contact-add');
+const bookTitle = document.querySelector('.title');
+const bookAuthor = document.querySelector('.author');
+const addBook = document.querySelector('.add_button');
+const pageTitle = document.querySelector('.awesome-books');
+const bookContainer = document.querySelector('.books-container');
+const contactContainer = document.querySelector('.contact-container');
+const list = document.querySelector('.list-text');
+const form = document.querySelector('.add-book');
+
+export {
+  container,
+  bookTitle,
+  addNew,
+  contact,
+  bookAuthor,
+  addBook,
+  pageTitle,
+  bookContainer,
+  contactContainer,
+  list,
+  form,
+};
